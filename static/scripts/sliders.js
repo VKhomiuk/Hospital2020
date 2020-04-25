e57e96e7abe2680a6fb7abf6d8f79ab275d6f7f0
Yurdjan <shchipskii@gmail.com>
2020-04-25 15:58:47 +0300

    slider_fix_1

--- static/scripts/sliders.js
+++ static/scripts/sliders.js
@@ -2,6 +2,8 @@ $('.slider').slick({
     autoplay: true,
     autoplaySpeed: 15000,
     arrows:true,
+    lazyLoad: 'progressive',
     prevArrow: '<div class="slickArrow slickPrev"><i class="im im-angle-left-circle"></i></div>',
     nextArrow: '<div class="slickArrow slickNext"><i class="im im-angle-right-circle"></i></div>',
+
 });
